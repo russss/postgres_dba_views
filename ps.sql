2bf3353ad21d1c6a1b72b9a256e131199cf03d8e
Russ Garrett <russ@garrett.co.uk>
2013-11-25 10:50:13 +0000

    Reflect state better

--- ps.sql
+++ ps.sql
@@ -3,8 +3,13 @@
 CREATE OR REPLACE VIEW ps AS
  SELECT pg_stat_activity.pid, pg_stat_activity.client_addr,
     pg_stat_activity.client_port, pg_stat_activity.usename,
-    now() - pg_stat_activity.xact_start AS age, pg_stat_activity.query,
-    pg_stat_activity.state, pg_stat_activity.waiting
+    now() - pg_stat_activity.xact_start AS age,
+    CASE WHEN pg_stat_activity.state = 'active'::text THEN
+      pg_stat_activity.query
+    ELSE
+      pg_stat_activity.state
+   END,
+   pg_stat_activity.waiting
    FROM pg_stat_activity
   WHERE pg_stat_activity.state <> 'idle'::text
   ORDER BY now() - pg_stat_activity.xact_start DESC;
